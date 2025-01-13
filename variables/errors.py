714792140ff1e8947bfe21e52f2615834ec1f241
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-01-13 08:58:52 +0300

    mod: errors part

--- variables/errors.py
+++ variables/errors.py
@@ -1,3 +1,10 @@
 mySuperMegaMessage ="Hello Every Student from Your Instructor Typjon!"
 
 print(mySuperMegaMessag)
+
+# E.g Traceback   
+# Traceback (most recent call last):
+#   File "c:\Users\Tazar\Desktop\tutorial_projects\Python_practices\variables\errors.py", line 3, in <module>
+#     print(mySuperMegaMessag)
+#           ^^^^^^^^^^^^^^^^^
+# NameError: name 'mySuperMegaMessag' is not defined. Did you mean: 'mySuperMegaMessage'?
