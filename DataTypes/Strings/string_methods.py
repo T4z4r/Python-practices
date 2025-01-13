9c0fadb0ce54749608ef2f004ad153ecbadc744e
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-01-13 10:03:07 +0300

    style: string_methods

--- DataTypes/Strings/string_methods.py
+++ DataTypes/Strings/string_methods.py
@@ -1,12 +1,12 @@
 # changing the string case
-name="This is the world's best python course"
-print ("--------------title()------------------")
+name = "this iS the worlDs best python course"
+print ("---------------title()------------------")
 # Method: title() ~ Each word starts with an upper case letter
 print (name.title())
-print ("----------------upper()----------------")
+print ("---------------upper()----------------")
 #Method" upper() - Each character is capitalized
 print(name.upper())
-print ("-----------------lower()---------------")
+print ("---------------lower()---------------")
 # Method" lower() - Each character is changed to lower case
 print (name.lower())
 
