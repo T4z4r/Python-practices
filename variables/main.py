53f45c9d720cf5656dbc4ca9a4d58df0849569f7
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-01-13 09:31:30 +0300

    feat: string assignment

--- variables/main.py
+++ variables/main.py
@@ -15,6 +15,16 @@ print(mySuperMessage)
 
 # Variable declarations rules
 message_1= "You can define variables like this."
+_message="This is also allowed."
+print(_message)
 # 1_message= "This is not allowd"
 # message 1="You cannot define variables with space"
-# Variable name can only contain letters, numbers and underscores
+
+
+# Rules
+ # 1:Variable name can only contain letters, numbers and underscores
+ # 2: Variable names cannot start with a number
+ # 3: Variable cannot be defined with spaces
+ # 4: Variable names are case-sensitive (myVariable and myvariable are different)
+ # 5: Variable names should be meaningful and descriptive
+ 
---
+++ variables/strings.py
@@ -0,0 +1,13 @@
+#  = ~This is an assignment operator
+userAge=19 
+a=5 # a=5 b=0
+print(a)
+b=10 # a=5 b=10
+print(a,b)
+a=b  #a=10 b=10
+print(a,b)
+b=a
+print(a,b)
+ 
+print("a=", a)
+print("b=", b)
