b0edef6a607b9e06d1efdec6ec18b4666e84e7f9
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-01-13 10:55:13 +0300

    feat: newlines and tabs

---
+++ new_lines_and_tabs.py
@@ -0,0 +1,16 @@
+#Whitespaces can be used to organize outputs
+
+#For Python Tabs
+print ("This is a \t python tab")
+
+#For Python Newlines
+print ("This is a \n New line")
+
+#For Printing Newlines without spaces
+print ("This is a New line\nwithout spaces\n")
+
+#Another Examples
+print("\tProgramming Languages :\nPython\nC++\nJava\nSwift")
+
+#For new lines with Tabs
+print("\tProgramming Languages :\n\t\tPython\n\t\tC++\n\t\tJava\n\t\tSwift") 
