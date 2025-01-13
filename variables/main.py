76896bcf6c9b293cd2182f8b2282f2dee1e22639
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-01-13 08:30:23 +0300

    first commit

---
+++ variables/main.py
@@ -0,0 +1,14 @@
+# For printing random string
+print("hello Typhon!")
+
+# For Printing vaiables
+
+# variable declarations
+mySuperMessage="Hello welcome to this python course"
+# For printing variables 
+print(mySuperMessage)
+
+mySuperMessage="This is the overrided message"
+
+# For printing variables after overriding
+print(mySuperMessage)
